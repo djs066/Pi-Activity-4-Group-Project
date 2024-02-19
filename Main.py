bbe3571ba41f528b5a2bcc38ea60adae9a3c286a
Daniel Sauer <dj_sauer05@hotmail.com>
2024-02-19 10:37:25 -0600

    code from professor so far

--- Main.py
+++ Main.py
@@ -3,13 +3,63 @@
 # Date: 2/19/24
 # Assignment: Pi activity 4, Simon Says
 ########################################
-import RPi.GPIO as GPIO
+
+import pineworkslabs.RPi as GPIO
 from time import sleep
-import pygame
+from random import choice
+import pygame                                       #Note: error is raised since pygame isnt included w/repository. should work if downloaded on machine
+from pygame.mixer import Sound
 
 #intialize pygame library
 pygame.init()
 
+GPIO.setmode(GPIO.LE_POTATO_LOOKUP)
+
+class Button:
+
+    def __init__(self, switch:int, led:int, sound:str, color:str):
+        self.switch = switch
+        self.led = led
+        self.sound: Sound = Sound(sound)
+        self.color = color
+        self.setupGPIO()
+
+    def setupGPIO(self):
+        GPIO.setup(self.switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.led, GPIO.OUT)
+
+    def turn_light_on(self):
+        GPIO.output(self.led, True)
+
+    def turn_light_off(self):
+        GPIO.output(self.led, False)
+
+    def is_pressed(self):
+        pass
+
+    def respond(self):      #Note: what it does when button/switch is pressed
+        pass
+    
+    def __str__(self):
+        return self.color
+
+
+class Simon:
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################
 # set the GPIO pin numbers
 # the LEDs (from L to R). Red, Blue, Yellow, Green
 leds = [6, 13, 19, 21 ]
